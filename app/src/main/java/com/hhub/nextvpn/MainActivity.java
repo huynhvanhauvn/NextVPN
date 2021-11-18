c6dc2b821dfc1544102ac6b9449d41d64f3c5bae
Huynh Van Hau <hau.huynh-van@banvien.com.vn>
2021-11-18 10:45:49 +0700

    Initial commit

---
+++ app/src/main/java/com/hhub/nextvpn/MainActivity.java
@@ -0,0 +1,110 @@
+package com.hhub.nextvpn;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.VpnService;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.Switch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import de.blinkt.openvpn.VpnProfile;
+import de.blinkt.openvpn.activities.DisconnectVPN;
+import de.blinkt.openvpn.core.ConfigParser;
+import de.blinkt.openvpn.core.OpenVPNService;
+import de.blinkt.openvpn.core.OpenVPNThread;
+import de.blinkt.openvpn.core.OpenVPNThreadv3;
+import de.blinkt.openvpn.core.ProfileManager;
+import de.blinkt.openvpn.core.VPNLaunchHelper;
+import de.blinkt.openvpn.core.VpnStatus;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OpenVPNService openVPNService;
+    private Switch aSwitch;
+    private OpenVPNService openVpnService;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    private ActivityResultLauncher<Intent> vpnLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        try {
+                            startOpenVpn();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        aSwitch = (Switch) findViewById(R.id.main_sw);
+
+        openVpnService = new OpenVPNService();
+        VpnStatus.initLogCache(getCacheDir());
+
+        aSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                prepareVpn();
+            } else {
+                stopOpenVPN();
+            }
+        });
+    }
+
+    private void prepareVpn() {
+        Intent intent = VpnService.prepare(getApplicationContext());
+
+        if (intent != null) {
+            vpnLauncher.launch(intent);
+        } else {
+            try {
+                startOpenVpn();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void startOpenVpn() throws Exception {
+        String config = OpenVPNHelper.getLocalOVPNConfig(getApplicationContext());
+        OpenVPNHelper.startOpenVPN(getApplicationContext(), config);
+    }
+
+    private void stopOpenVPN() {
+        OpenVPNHelper.stopOpenVPN(getApplicationContext());
+    }
+}
---
+++ app/src/main/java/com/hhub/nextvpn/OpenVPNHelper.java
@@ -0,0 +1,67 @@
+package com.hhub.nextvpn;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import de.blinkt.openvpn.VpnProfile;
+import de.blinkt.openvpn.activities.DisconnectVPN;
+import de.blinkt.openvpn.core.ConfigParser;
+import de.blinkt.openvpn.core.ProfileManager;
+import de.blinkt.openvpn.core.VPNLaunchHelper;
+
+public class OpenVPNHelper {
+
+    public static String getLocalOVPNConfig(Context context) {
+        String config = "";
+        String line;
+        InputStream conf = null;
+        try {
+            conf = context.getAssets().open("default.ovpn");
+            InputStreamReader isr = new InputStreamReader(conf);
+            BufferedReader br = new BufferedReader(isr);
+            while (true) {
+                line = br.readLine();
+                if (line == null) break;
+                config += line + "\n";
+            }
+
+            br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return config;
+    }
+
+    public static void startOpenVPN(Context context, String config) throws RemoteException {
+        ConfigParser cp = new ConfigParser();
+        try {
+            cp.parseConfig(new StringReader(config));
+            VpnProfile vp = cp.convertProfile();// Analysis.ovpn
+            vp.mName = "myvpn";
+            vp.mProfileCreator = context.getPackageName();
+            vp.mUsername = "vpn";
+            vp.mPassword = "vpn";
+            ProfileManager.setTemporaryProfile(context, vp);
+            VPNLaunchHelper.startOpenVpn(vp, context);
+            Log.d("hhlog1", "launch");
+        } catch (IOException | ConfigParser.ConfigParseError e) {
+            Log.d("hhlog1", e.getMessage());
+            throw new RemoteException(e.getMessage());
+        }
+    }
+
+    public static void stopOpenVPN(Context context) {
+        Intent disconnectVPN = new Intent(context, DisconnectVPN.class);
+        disconnectVPN.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(disconnectVPN);
+    }
+}
